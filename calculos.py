32fb93db1b0c804ae453f61bf1aa28ffac311157
6ust <gust.guedes95@gmail.com>
2019-09-28 12:44:22 -0300

    Formula para gerar a tabuada de um numero informado

--- calculos.py
+++ calculos.py
@@ -1,4 +1,14 @@
 # Gerar Tabuada
+contador = 1
+
+print("Informe o numero da tabuada a ser gerada")
+num_tabuada = int(input())
+
+while(contador <= 10):
+    resultado = num_tabuada * contador
+    print(" {} * {} = {}" .format(num_tabuada, contador, resultado))
+    contador = contador + 1
+
 
 
 # Formula de Baskara
