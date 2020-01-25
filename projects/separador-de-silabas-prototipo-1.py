03f87bf130036715847496c56a1fc42fbf05f2bf
6ust <gust.guedes95@gmail.com>
2020-01-25 17:06:14 -0200

    Atualizando projeto de separação de sílabas PT-BR: [separador-de-silabas-prototipo-1.py]

--- projects/separador-de-silabas-prototipo-1.py
+++ projects/separador-de-silabas-prototipo-1.py
@@ -1,45 +1,208 @@
-wordSlb = []
-sepSlb = []
-c = []
-v = []
+# Author: Gustavo Guedes - 6ust <- [Git]
 
-word = "BARUERI"
+def jumpLine(amount):
+	counterAmount = 0
+	while(counterAmount < amount):
+		print()
+		counterAmount += 1
 
-cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
-vog = ["a", "e", "i", "u"]
+# PALAVRA TESTADA
+# word = "DESÇAM"
+# word = "BARUERI"
 
 
-lenSW = 0
-for linhaSilaba in range(len(word)):
-	for colunaSilaba in range(1):
-		
-		for consProc in range(len(cons)):
+# PALAVRA TESTE
+# word = "AMOR"
+# word = "LEAO"
+# word = "CAMALEAO"
+
+# PALAVRA A ESCOLHA DO USUARIO
+word = input("\n\nInforme uma palavra padrao de palavras aceitas: \nNAO COMECE COM VOGAL \nNAO SEJA PLURAL \nNAO TERMINE COM CONSOANTE:\n")
+word = word.upper()
+
+
+# CONTADORES
+counterWord = 1
+
+# ARRAY DE INDEX
+consonantId = []
+vowelId = []
+digraphId = []
+
+# ARRAY DE SILABAS
+wordSyllableSep = []
+separatorSyllableIds = []
+
+
+# --------------------------------------------
+# REGRAS DE SEPARAÇÃO DE SILABA
+# --------------------------------------------
+# SEPARAR QUANDO
+digraphs = ["rr", "ss", "sc", "sç", "xc"]
+consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "ç"]
+vowel = ["a", "e", "i", "o" , "u", "y"]
+
+# NÃO SEPARAR QUANDO
+
+# ********************************************
+
+jumpLine(1)
+print("---------------------------------")
+print("--- AMBIENTE DE TESTES 002 ------")
+print("---------------------------------")
+
+jumpLine(1)
+print("PALAVRA => " + word)
+jumpLine(1)
+
+# --------------------------------------------
+# ANALISE INDEX DE SEPARAÇÃO
+# --------------------------------------------
+lenSyllableWord = 0
+
+# PERCORRE ARRAY [WORD]
+for lineWord in range(len(word)):
+	for columnWord in range(1):
+
+		# PERCORRE ARRAY DE ANALISE DE digraphs
+		for digraphsSearchAdjust in range(len(digraphs)):
+			# REGRA PARA NÃO LER A MAIS, [M_] POSIÇÃO NÃO EXISTENTE 
+			if lineWord < (len(word) - 1):
+
+				# IGUAL A DIGRAFO INICIO
+				if (word[lineWord:][:2] == digraphs[digraphsSearchAdjust].upper()):
+					digraphId.append(lineWord)
+					print("Digrafo encontrado -> " + str(word[lineWord:][:2]))
+
+		# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+		# PERCORRE ARRAY DE ANALISE DE CONSOANTES
+		for consonantSearch in range(len(consonant)):
 			
 			# IGUAL A CONSOANTE INICIO
-			if (word[linhaSilaba] == cons[consProc].upper()):
-				c.append(linhaSilaba)
-				lenSW += 1
+			if (word[lineWord] == consonant[consonantSearch].upper()):
+				consonantId.append(lineWord)
+				print("Consoante encontrada -> " + str(word[lineWord]))
+				# lenSyllableWord += 1
 
-		for vogProc in range(len(vog)):
-			if (word[linhaSilaba] == vog[vogProc].upper()):
-				v.append(linhaSilaba)
-				lenSW += 1
+		# SEPARADOR DE VOGAIS
+		for vowelSearch in range(len(vowel)):
 
-v.pop(1)
-# print(v)
+			# IGUAL A VOGAL INICIO
+			if (word[lineWord] == vowel[vowelSearch].upper()):
+				vowelId.append(lineWord)
+				print("Vogal encontrada -> " + str(word[lineWord]))
+				# lenSyllableWord += 1
 
-for linhaSW in range(3):
-	for colunaSW in range(2):
-		if (colunaSW == 0):
-			sepSlb.append([linhaSW])
-			sepSlb[linhaSW].append(c[linhaSW])
-			sepSlb[linhaSW].append((v[linhaSW] - c[linhaSW]) + 1)
+		# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-print()
-print("Posições das silabas no array [indexInicialSilaba, consoante, vogal] --> " + str(sepSlb))
-print()
 
-# MOSTRAR PALAVRA EM SILABAS
-for linhaShow in range(3):
-	for colunaShow in range(1):
-		print(word[ sepSlb[linhaShow][1] :][: sepSlb[linhaShow][2] ])
+# --------------------------------------------
+# REMOVENDO REPETIÇÃO DE VOGAIS
+# --------------------------------------------
+for vowelIndex in range(len(vowelId)):
+	if (vowelIndex < len(vowelId) - 1):
+		# print(str(vowelId[vowelIndex + 1]) + " - " + str(vowelId[vowelIndex]) + " = " + str((vowelId[vowelIndex + 1] - vowelId[vowelIndex])) + " ====>" + str((vowelId[vowelIndex + 1] - vowelId[vowelIndex]) == 1))
+		if (vowelId[vowelIndex + 1] - vowelId[vowelIndex]) == 1:
+			vowelId.pop(vowelIndex)
+
+# ********************************************
+
+			
+# --------------------------------------------
+# GERAR SILABAS DA PALAVRA
+# --------------------------------------------
+jumpLine(1)
+print("-------------------------------------------------------------------------")
+print("--- Gerando silaba da palavra --> " + str(word))
+print("-------------------------------------------------------------------------")
+jumpLine(1)
+
+# CASO A PALAVRA NAO COMECE COM VOGAL/ NAO SEJA PLURAL/ NAO TERMINE COM CONSOANTE
+if (len(consonantId) == len(vowelId)):
+	for consonantIndex in range(len(consonantId)):	
+		print("incluindo intervalo de sílaba ------> [ " + str(consonantId[consonantIndex]) + " - " + str(vowelId[consonantIndex]) + " ]")
+		for startAndEnd in range(2):
+			if startAndEnd == 0:
+				separatorSyllableIds.append([consonantIndex])
+				separatorSyllableIds[consonantIndex].append(consonantId[consonantIndex])
+				separatorSyllableIds[consonantIndex].append((vowelId[consonantIndex] - consonantId[consonantIndex]) + 1)
+	jumpLine(1)
+
+
+# else:
+# 	for vowelIndex in range(len(vowelId)):
+# 		print("Incl V ------>" + str(consonantId[vowelIndex]) + " - " + str(vowelId[vowelIndex]))
+# 		separatorSyllableIds.append(vowelId[vowelIndex])
+# 		# separatorSyllableIds.append(consonantId[consonantIndex])
+
+print("SEPARATOR SYLLABLE IDS ==> " + str(separatorSyllableIds))
+jumpLine(1)
+print("****************************************")
+# ********************************************
+
+
+# -----------------------------------------------------
+# VIZUALIZAÇÃO DE INDEXES PARA REALIZAÇÃO DE TESTES
+# -----------------------------------------------------
+
+# jumpLine(1)
+# print("CONSONANTS")
+# print(consonantId)
+
+# jumpLine(1)
+# print("VOWELS")
+# print(vowelId)
+
+# ********************************************
+
+
+# --------------------------------------------
+# ORGANIZAÇÃO DE SEPARAÇÃO DE SILABAS - REGRA
+# --------------------------------------------
+# for consonantIndex in range(len(consonantId)):
+# 	print("CONS --------> "  + str(word[consonantId[consonantIndex]]))
+# 	for vowelIndex in range(len(vowelId)):
+# 		print("VOG ---> "  + str(word[vowelId[vowelIndex]]))
+# 		print()
+# 		# separatorSyllableIds.append(consonantIndex)
+
+
+# ********************************************
+
+
+# ------------------------------------------------
+# VISUALIZAÇÃO DE INDEX PARA REALIZAÇÃO DE TESTES
+# ------------------------------------------------
+jumpLine(2)
+print("===================================")
+print("=== ORGANIZAÇÃO ===================")
+print("===================================")
+jumpLine(1)
+
+print("DIGRAPHS ==> " + str(digraphId))
+print("VOWEL ==> " + str(vowelId))
+print("CONSONANT ==> " + str(consonantId))
+jumpLine(1)
+print("SEPARATOR SYLLABLE IDS ==> " + str(separatorSyllableIds))
+jumpLine(1)
+print("****************************************")
+
+# print("SEPARATOR LEN ==> " + str(len(separatorSyllableIds)))
+
+# VISUALIZAÇÃO DAS SILABAS
+jumpLine(1)
+print("---------------------------------")
+print("--- VISUALIZAÇÃO DE SEPARAÇÃO ---")
+print("---------------------------------")
+jumpLine(1)
+print(word)
+jumpLine(1)
+
+for separatorSyllableIndex in range(len(separatorSyllableIds)):	
+	for startAndEnd in range(1):
+		print(str( word[ separatorSyllableIds[separatorSyllableIndex][1] :] [: (separatorSyllableIds[separatorSyllableIndex][2])] ))
+		# print("COLUNA" + str(separatorSyllableIds[separatorSyllableIndex][2]))
+		# print( word[separatorSyllableIndex :] [: startAndEnd] )
+
+print("****************************************")
